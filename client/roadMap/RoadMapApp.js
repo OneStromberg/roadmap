fbb0643a34aaaf2440aaef82586606ec4aa334fc
fitzsyke <fitzsyke@gmail.com>
2017-02-08 16:21:16 +0200

    Save ui-kit

--- client/roadMap/RoadMapApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RoadMap from './RoadMap';
-
-require('./../../common/styles/styles');
-require('./../common/styles/styles');
-require('./styles/styles.less');
-
-ReactDOM.render(<RoadMap />, document.getElementById('root'));
