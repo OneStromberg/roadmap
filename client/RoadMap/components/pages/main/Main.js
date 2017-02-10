1aa3497a0d73ded953803bd4c8940848a671314f
ievgen golovnov <93ave.blues@gmail.com>
2017-02-10 15:49:36 +0200

    commit

--- client/RoadMap/components/pages/main/Main.js
+++ client/RoadMap/components/pages/main/Main.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {AppBar, View} from 'components';
+import {AppBar, Container, Header, Text, View} from 'components';
 import {ColorPaletteContainer} from './../../partials/';
+import Page from './../../../../common/components/Page';
 
 class ComponentsApp extends React.Component {
     render(){
         return(
             <View>
                 <AppBar />
-                <ColorPaletteContainer />
+                <Page>
+                    <Container>
+                        <Header>
+                            <Text> 01 </Text>
+                            <Text> Color Palette </Text>
+                        </Header>
+                        <ColorPaletteContainer />
+                    </Container>
+                </Page>
             </View>
         )
     };
