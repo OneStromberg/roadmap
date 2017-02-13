756ac00a3ca3deb0691e04aafe7b9bb0cf36ba75
ievgen golovnov <93ave.blues@gmail.com>
2017-02-13 20:32:23 +0200

    commit

--- client/RoadMap/components/partials/TypographyCard.js
+++ client/RoadMap/components/partials/TypographyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Component, Input, View} from 'components';
+import {Card, Component, Label, Input, View} from 'components';
 
 class TypographyCard extends Component {
     render(){
@@ -8,7 +8,7 @@ class TypographyCard extends Component {
                 {this.props.title}
                 <Label>font size: {this.props.fontSize}</Label>
                 <Label>color: {this.props.color}</Label>
-                <Label>line-height: {this.props.lineHeight}</Label>
+                <Label>line-height: {this.props.color}</Label>
                 <Label>font-weight: {this.props.fontWeight}</Label>
             </Card>
         )
