fefb95a491d899ff98be77eb8e6a433e37283938
ievgen golovnov <93ave.blues@gmail.com>
2017-02-13 19:23:35 +0200

    commit

--- client/RoadMap/components/partials/TypographyCard.js
+++ client/RoadMap/components/partials/TypographyCard.js
@@ -5,23 +5,11 @@ class TypographyCard extends Component {
     render(){
         return(
             <Card className="typography-card">
-                <View className="">
-                    <Label>Input Empty</Label>
-                    <Input type="text"/>
-                    <Label>Input Selected</Label>
-                    <Input type="text"/>
-                    <Label>Input Typing</Label>
-                    <Input type="text"/>
-                    <Label>Input Error</Label>
-                    <Input />
-                </View>
-                <View className="">
-                    <Label></Label>
-                    <Text></Text>
-
-                </View>
-                <View className="">
-                </View>
+                {this.props.title}
+                <Label>font size: {this.props.fontSize}</Label>
+                <Label>color: {this.props.color}</Label>
+                <Label>line-height: {this.props.lineHeight}</Label>
+                <Label>font-weight: {this.props.fontWeight}</Label>
             </Card>
         )
     }
--- client/RoadMap/components/partials/TypographyContainer.js
+++ client/RoadMap/components/partials/TypographyContainer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Component, View} from 'components';
+import TypographicCard from './TypographyCard';
 
 class TypographyContainer extends Component {
     render(){
         return(
             <View className="typography-container">
+                <TypographicCard title={<Title className="lbl-1"></Title>}/>
+                <TypographicCard title={<Title className="lbl-2"></Title>}/>
+                <TypographicCard title={<Title className="lbl-3"></Title>}/>
             </View>
         )
     }
