ddb9164df57fbbf2f3795d3cbf290bc11585e736
fitzsyke <fitzsyke@gmail.com>
2017-02-13 13:40:40 +0200

    Typography components

---
+++ client/MultiBit/components/partials/TypographyCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Component, View, Title, Subtitle} from 'components';
+
+class TypographyCard extends Component {
+    render(){
+        return(
+            <View className="typography-card">
+				<Title>Text 1 - Price Over Time</Title>
+				<Subtitle>SF UI Display Regular, Size 16, Character 0, Color #BBBED7</Subtitle>
+            </View>
+        )
+    }
+}
+
+export default TypographyCard;
+
