543eb4ebd9103592fe84db2feecc56990b31bf85
fitzsyke <fitzsyke@gmail.com>
2017-02-15 11:19:11 +0200

    webp

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     entry:{
         common:['babel-polyfill','react','redux'],
-        guide:["./client/RoadMap/RoadMapApp"]
+        guide:["./client/MultiBit/MultiBitApp"]
     },
     output: {
         path: __dirname + '/public/build/',
