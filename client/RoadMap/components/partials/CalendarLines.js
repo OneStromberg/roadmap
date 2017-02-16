946ca464dd456d8bf94c9ee6a34af4820ed5f964
ievgen golovnov <93ave.blues@gmail.com>
2017-02-16 19:14:22 +0200

    commit

--- client/RoadMap/components/partials/CalendarLines.js
+++ client/RoadMap/components/partials/CalendarLines.js
@@ -4,7 +4,7 @@ import {Component, ViewColor} from 'components';
 class CalendarLines extends Component {
     render(){
         return(
-            <ViewColor background={this.props.background} className="calendar-lines-view">
+            <ViewColor background={this.props.background} className={this.getClassName(this.props)}>
             {this.props.leftComponent}
             {this.props.rightComponent}
             </ViewColor>
