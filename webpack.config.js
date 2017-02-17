caf8ed25460e9096a1cbc1fea0f94e31db67a0ce
ievgen golovnov <93ave.blues@gmail.com>
2017-02-17 23:46:43 +0200

    commit -m "Refactoring"

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     entry:{
         common:['babel-polyfill','react','redux'],
-        guide:["./client/MultiBit/MultiBitApp"]
+        guide:["./client/RoadMap/RoadMapApp"]
     },
     output: {
         path: __dirname + '/public/build/',
