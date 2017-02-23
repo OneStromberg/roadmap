a9f5486ae1d6856d370e5bf5ea63ebc2cde65e37
ievgen golovnov <93ave.blues@gmail.com>
2017-02-23 12:15:33 +0200

    commit

--- client/RoadMap/components/partials/ElementsContainer.js
+++ client/RoadMap/components/partials/ElementsContainer.js
@@ -1,15 +1,86 @@
 import React from 'react';
-import {Component, Title, View} from 'components';
-import ElementsCard from './ElementsCard';
+import {Card, CardHeader, Component, Label, Image, Title, Text, View} from 'components';
 
 class ElementsContainer extends Component {
     render(){
         return(
             <View className="elements-container">
-                <ElementsCard />
-                <ElementsCard />
-                <ElementsCard />
-                <ElementsCard />
+                <View className="elements-card">
+                    <Card header={
+                        <CardHeader>
+                            <Label>Ecoplay</Label> 
+                            <Image /> 
+                            <Label>Gamebox</Label>
+                        </CardHeader>}>
+                        <View className="elements-card-footer">
+                            <Label>5</Label>
+                            <Label>step</Label>
+                        </View>
+                        <View className="elements-card-footer">
+                            <Label >2</Label>
+                            <Label>done</Label>
+                        </View>
+                    </Card>
+                </View>
+                <View className="elements-card">
+                    <Card header={
+                        <CardHeader>
+                            <Label>Drive</Label> 
+                            <Image /> 
+                            <Label>Google</Label>
+                        </CardHeader>}>
+                        <View className="elements-card-footer">
+                            <Label>5</Label>
+                            <Label>step</Label>
+                        </View>
+                        <View className="elements-card-footer">
+                            <Label>2</Label>
+                            <Label>done</Label>
+                        </View>
+                    </Card>
+                </View>
+                <View className="elements-card">
+                    <Card header={
+                        <CardHeader>
+                            <Label>Test</Label> 
+                            <Image /> 
+                            <Label>Coca-Cola</Label>
+                        </CardHeader>}>
+                        <View>
+                            <Label>No active</Label>
+                            <Image />
+                        </View>
+                    </Card>
+                </View>
+                <View className="elements-card">
+                    <Card header={
+                        <CardHeader>
+                            <Label>Test</Label> 
+                            <Image /> 
+                            <Label>Coca-Cola</Label>
+                        </CardHeader>}>
+                        <View>
+                            <Label>Pending</Label>
+                            <Label>...</Label>
+                        </View>
+                    </Card>
+                </View>
+                <View className="elements-card-large">
+                    <Card>
+                        <Label>About project</Label>
+                        <Text>Consideration should be given to the existing applicable literature, problems and successes associated with existing solutions, costs, and marketplace needs.</Text>
+                    </Card>
+                </View>
+                <View className="elements-card-medium">
+                    <Card>
+                        
+                    </Card>
+                </View>
+                <View className="elements-card-small">
+                    <Card>
+                        
+                    </Card>
+                </View>
             </View>
         )
     }
