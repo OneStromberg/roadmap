6ae52ffc6c1da40e8b1d3777de7df819585f6e57
salex <stro@zajno.com>
2017-03-10 17:32:52 +0200

    pages adding

--- client/MultiBit/routes/mutlibit.js
+++ client/MultiBit/routes/mutlibit.js
@@ -5,7 +5,14 @@ import MultibitWrapper from './../components/templates/MultibitWrapper';
 import Login from './../components/pages/login';
 import Pincode from './../components/pages/Pincode';
 import CreateAccKeywordsFilled from './../components/pages/createAccKeywordsFilled';
+
 import Overview from './../components/pages/overview';
+import Transact from './../components/pages/transact';
+import Exchange from './../components/pages/exchange';
+import Contacts from './../components/pages/contacts';
+import History  from './../components/pages/history';
+import Settings from './../components/pages/settings';
+import Help     from './../components/pages/help';
 
 import { Routes } from './../constants';
 
@@ -17,7 +24,13 @@ const styleRoute = (
         <Route path={Routes.LOGIN} component={Login} />
         <Route path={Routes.PINCODE} component={Pincode} />
         <Route component={AppWindow}>
-            <Route path={Routes.OVERVIEW} component={Overview} />
+            <Route path={Routes.OVERVIEW}   component={Overview} />
+            <Route path={Routes.TRANSACT}   component={Transact} />
+            <Route path={Routes.EXCHANGE}   component={Exchange} />
+            <Route path={Routes.CONTACTS}   component={Contacts} />
+            <Route path={Routes.HISTORY}    component={History} />
+            <Route path={Routes.SETTINGS}   component={Settings} />
+            <Route path={Routes.HELP}       component={Help} />
         </Route>
         <Route path={Routes.CREATEACC_KEYWORDS_FILLED} component={CreateAccKeywordsFilled} />
      </Route>
